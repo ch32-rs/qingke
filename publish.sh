73a21d1b527327c17bd80b8722d66e10a4fcd46e
Andelf <andelf@gmail.com>
2024-11-03 12:58:02 +0800

    fix: use riscv32imc as default target

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-cargo publish  --target riscv32imac-unknown-none-elf --allow-dirty
+cargo publish  --target riscv32imc-unknown-none-elf --allow-dirty
 
-(cd qingke-rt/macros && cargo publish --target riscv32imac-unknown-none-elf --allow-dirty)
+(cd qingke-rt/macros && cargo publish --target riscv32imc-unknown-none-elf --allow-dirty)
 
-(cd qingke-rt && cargo publish --target riscv32imac-unknown-none-elf --allow-dirty)
+(cd qingke-rt && cargo publish --target riscv32imc-unknown-none-elf --allow-dirty)
