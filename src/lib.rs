75dbd9539d5abf66f24435b66da3a02bb251dde6
Codetector <codetector@codetector.org>
2024-11-02 17:42:22 -0700

    feat: introduce compile error if using atomic
    This change introduce a compile error if the toolchain is using a target
    with atomics. It looks like at least on the the QingKe V4 atomics are
    suspiciously broken.
    
    There is also a feature you can enable
    `unsafe-trust-wch-atomics` you can enable to remove
    this compile error
    
    See issue: https://github.com/ch32-rs/ch32-hal/issues/59

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,22 @@ pub mod register;
 // re-export
 pub use riscv;
 
+#[cfg(all(
+    any(
+        target_has_atomic = "8",
+        // target_has_atomic = "16",
+        // target_has_atomic = "32",
+        // target_has_atomic = "64",
+        // target_has_atomic = "128",
+        // target_has_atomic = "ptr"
+    ),
+    not(feature = "unsafe-trust-wch-atomics")
+))]
+compile_error!(
+    "As tested on QingKe V4, most likely the atomics are broken, 
+please validate the atomic instruction on the hardware using 
+something like litmus test suite before trusting them."
+);
+
 #[cfg(feature = "critical-section-impl")]
 mod critical_section_impl;
